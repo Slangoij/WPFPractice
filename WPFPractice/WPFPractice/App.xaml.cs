d3e0f9b8b9b5165fe7aa76fca380b5fdff157650
ijjeong <ijjeong@piegroup.co.kr>
2025-04-20 09:35:08 +0900

    WPF로 PC카카오톡 만들기 3. 의존성 주입
    의존성 주입 개념과 실 코드는 추가 공부 필요할 듯

---
+++ WPFPractice/WPFPractice/App.xaml.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+using WPFPractice.Services;
+
+namespace WPFPractice
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            Services = ConfigureServices();
+            Startup += App_Startup;
+        }
+
+        private void App_Startup(object sender, StartupEventArgs e)
+        {
+            var mainView = App.Current.Services.GetService<MainWindow>();
+            mainView.Show();
+        }
+
+        /// <summary>
+        /// Gets the current <see cref="App"/> instance in use
+        /// </summary>
+        public new static App Current => (App)Application.Current;
+
+        /// <summary>
+        /// Gets the <see cref="IServiceProvider"/> instance to resolve application services.
+        /// </summary>
+        public IServiceProvider Services { get; }
+
+        /// <summary>
+        /// Configures the services for the application.
+        /// </summary>
+        private static IServiceProvider ConfigureServices()
+        {
+            var services = new ServiceCollection();
+
+            // Services
+            services.AddSingleton<ITestService, TestService>();
+
+            // Views
+            services.AddSingleton<MainWindow>();
+
+            return services.BuildServiceProvider();
+        }
+    }
+
+}
---
+++ WPFPractice/WPFPractice/Services/TestService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFPractice.Services
+{
+    public class TestService : ITestService
+    {
+        public string GetString()
+        {
+            return "안냐세요.";
+        }
+    }
+}
